789856c4631db2399969db9635e2916de511897d
Clara Ogalla <claraogalla@gmail.com>
2022-07-13 11:02:10 +0200

    Electrodomestico v0.2

--- UD08_03/src/Electrodomestico/Electrodomestico.java
+++ UD08_03/src/Electrodomestico/Electrodomestico.java
@@ -1,7 +1,11 @@
 package Electrodomestico;
 
 public class Electrodomestico {
-
+	private double precioBase = 100;
+	private String color = "Blanco";
+	private char consumoElectrico = 'F';
+	private double peso = 10;
+	
 	public Electrodomestico() {
 		// TODO Auto-generated constructor stub
 		
