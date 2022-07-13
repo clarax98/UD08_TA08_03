12fa23e2502ddc0edb1263b731039e7349fc81aa
Clara Ogalla <claraogalla@gmail.com>
2022-07-13 11:02:30 +0200

    Electrodomestico v0.2

--- UD08_03/src/Electrodomestico/Electrodomestico.java
+++ UD08_03/src/Electrodomestico/Electrodomestico.java
@@ -11,4 +11,74 @@ public class Electrodomestico {
 		
 	}
 
+	public Electrodomestico(double precioBase, double peso) {
+		super();
+		this.precioBase = precioBase;
+		this.peso = peso;
+	}
+
+	public Electrodomestico(double precioBase, String color, char consumoElectrico, double peso) {
+		super();
+		this.precioBase = precioBase;
+		this.color = color;
+		this.consumoElectrico = consumoElectrico;
+		this.peso = peso;
+	}
+
+	/**
+	 * @return the precioBase
+	 */
+	public double getPrecioBase() {
+		return precioBase;
+	}
+
+	/**
+	 * @param precioBase the precioBase to set
+	 */
+	public void setPrecioBase(double precioBase) {
+		this.precioBase = precioBase;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	/**
+	 * @return the consumoElectrico
+	 */
+	public char getConsumoElectrico() {
+		return consumoElectrico;
+	}
+
+	/**
+	 * @param consumoElectrico the consumoElectrico to set
+	 */
+	public void setConsumoElectrico(char consumoElectrico) {
+		this.consumoElectrico = consumoElectrico;
+	}
+
+	/**
+	 * @return the peso
+	 */
+	public double getPeso() {
+		return peso;
+	}
+
+	/**
+	 * @param peso the peso to set
+	 */
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
 }
